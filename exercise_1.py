dc0e7c1d7cd7a7ee9c5c4d8a07e03313b5f15168
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2020-10-31 14:11:37 +0300

    Update and rename hello_world.py to exercise_1.py
    printing stuff

---
+++ exercise_1.py
@@ -0,0 +1,22 @@
+print("Hello World!")
+
+print("Hello, what is your name?")
+input()
+
+print(type("python"))
+
+print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!")
+print("\t\tUp above the world so high,\n\t\tLike a diamond in the sky.")
+print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!  ")
+
+#expected output
+#Hello World!
+#Hello, what is your name?
+#Dogukan
+#<class 'str'>
+#Twinkle, twinkle, little star,
+#	How I wonder what you are!
+#		Up above the world so high,
+#		Like a diamond in the sky.
+#Twinkle, twinkle, little star,
+#	How I wonder what you are!  
