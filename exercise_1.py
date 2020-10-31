75901b1f65af2904e479ea4443c9838a22bbbb92
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2020-10-31 15:02:16 +0300

    Update exercise_1.py
    declaring variables and printing stuff

--- exercise_1.py
+++ exercise_1.py
@@ -9,7 +9,23 @@ print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!")
 print("\t\tUp above the world so high,\n\t\tLike a diamond in the sky.")
 print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!  ")
 
-#expected output
+#variables
+num1 = 8
+num2 = 14
+print("Number 1 is:",num1)
+print("Number 2 is:",num2)
+num3 = num1 * num2 #math operation
+print("Multiplication of the two number is:",num3)
+
+num4 = int(input("Please enter a number: "))
+print(type(num4))
+print("The number you have entered is:",num4)
+num5 = int(input("Please enter another number: "))
+print(type(num5))
+print("The second number you have entered is:",num5)
+print("Sum of the two numbers you have entered is:",num4+num5)
+
+#Expected Output
 #Hello World!
 #Hello, what is your name?
 #Dogukan
@@ -20,3 +36,13 @@ print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!  ")
 #		Like a diamond in the sky.
 #Twinkle, twinkle, little star,
 #	How I wonder what you are!  
+#Number 1 is: 8
+#Number 2 is: 14
+#Multiplication of the two number is: 112
+#Please enter a number: 12
+#<class 'int'>
+#The number you have entered is: 12
+#Please enter another number: 41
+#<class 'int'>
+#The second number you have entered is: 41
+#Sum of the two numbers you have entered is: 53
