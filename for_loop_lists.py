56955a6075a5db0de95b9e42dc3d53798fb493b9
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2020-11-19 23:30:53 +0300

    Create for_loop_lists.py
    using for loop with a list that has tuples

---
+++ for_loop_lists.py
@@ -0,0 +1,15 @@
+mylist = [(1,2),(3,4),(5,6),(7,8),(9,10)]
+
+print("Lenght of my list is:",len(mylist))
+print("Elements in my list are below:")
+
+for (a,b) in mylist:
+    print(a,b)
+"""
+Lenght of my list is: 5
+Elements in my list are below:
+1 2
+3 4
+5 6
+7 8
+9 10 """
