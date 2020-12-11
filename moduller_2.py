7229dc91a7bdf2a66fb372c522124e63e7fecd86
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2020-12-11 13:07:12 +0300

    Create moduller_2.py

---
+++ moduller_2.py
@@ -0,0 +1,15 @@
+#modüller-2
+from math import*
+print(pow(2,5)) #power of 2 by 5
+print(sqrt(16)) #square root of 16
+print(cos(45))
+
+a = int(input("Üçgenin dik kenarlardan birini cm olarak giriniz:"))
+b = int(input("Üçgenin dik kenarlardan digerini cm olarak giriniz:"))
+c = sqrt(pow(a,2)+pow(b,2))
+print("Üçgenin hipotenüsü",c,"cm uzunluktadir")
+
+"""
+Burada matematiksel modüller kullanılarak işlemler yapılmıştır
+pek çok matematiksel fonksiyon python içerisinde bulunmaktadır
+"""
