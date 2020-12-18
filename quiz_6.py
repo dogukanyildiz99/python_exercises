f3b728472cc356383a3a2fa6177bbc13ebee9ed6
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2020-12-18 16:06:13 +0300

    Add files via upload

---
+++ quiz_6.py
@@ -0,0 +1,17 @@
+#Dogukan YILDIZ 20360859095
+def  ucgenmi(a,b,c):
+    if a < (b + c) and a > abs(b-c): #abs absolute fonksiyonudur mutlak degeri temsil eder
+        print("Values you have entered does represent a triangle.")
+    elif abs(c-a) < b < (c + a):
+        print("Values you have entered does represent a triangle.")
+    elif abs(a-b) < c < (a + b):
+        print("Values you have entered does represent a triangle.")
+    else:
+        print("Values you have entered does NOT represent a triangle.")
+    return (a,b,c)
+
+a = int(input("Please enter an integer value for a:"))
+b = int(input("Please enter an integer value for b:"))
+c = int(input("Please enter an integer value for c:"))
+
+ucgenmi(a,b,c)
